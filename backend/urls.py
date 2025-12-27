7a31879f85ee4b992781c169461ea7ece8cea460
saikrishnabit2003-star <saikrishnabit2003@gmail.com>
2025-12-27 15:20:44 +0530

    urls changes

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path, include
 from django.http import JsonResponse
+from stacov import urls
 
 def health(request):
     return JsonResponse({"status": "ok", "service": "django-api"})
 
 urlpatterns = [
     path("", health),
-    path("api/", include("stacov.urls")),
+    path("api/", urls),
 ]
