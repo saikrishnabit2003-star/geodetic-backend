cf3db6f8cb366599a28e5991744a714b39be52f7
saikrishnabit2003-star <saikrishnabit2003@gmail.com>
2025-12-27 14:27:44 +0530

    version 2.0

--- backend/urls.py
+++ backend/urls.py
@@ -1,21 +1,11 @@
-"""
-URL configuration for backend project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok", "service": "django-api"})
 
 urlpatterns = [
-    path('api/', include('stacov.urls')),  # STACOV API endpoints
+    path("", health),
+    path("stacov/", include("stacov.urls")),
 ]
